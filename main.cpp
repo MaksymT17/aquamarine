8b94a409a8066c397c02df29c59239ebc04849c4
Maksym Vinnichek <m.vinnichek@gmail.com>
2021-11-08 10:16:26 +0200

    Add posibility to pass user-defined parameters

--- main.cpp
+++ main.cpp
@@ -13,18 +13,38 @@
 #include <memory>
 #include <stdio.h>
 
-int main() 
+int main(int argc, char * argv[]) 
 {
 	using namespace am::common::types;
 	using namespace am::analyze;
 
-	std::shared_ptr<am::common::Logger> loggerPtr(
-		new am::common::Logger("log.log"));
+	std::shared_ptr<am::common::Logger> loggerPtr(std::make_shared<am::common::Logger>("log.log"));
 
 	am::extraction::MultipleBmpExtractor extractor(loggerPtr);
-	std::string base("inputs/rs_1.bmp");
-	std::string toCompare("inputs/rs_2.bmp");
-	std::vector<std::string> fileNames = { base, toCompare };
+
+	std::string base_img_path;
+	std::string cmp_img_path;
+	const char* conf_path;
+	const char* out_img_path;
+
+	if (argc == 5)
+	{
+		std::cout << "Parse user-defined parameters." << std::endl;
+		base_img_path = std::string(argv[1]);
+		cmp_img_path = std::string(argv[2]);
+		conf_path = argv[3];
+		out_img_path = argv[4];
+	}
+	else
+	{
+		std::cout << "Use default parameters" << std::endl;
+		base_img_path = "inputs/rs_1.bmp";
+		cmp_img_path = "inputs/rs_2.bmp";
+		conf_path = "inputs/configuration.csv";
+		out_img_path = "compare_result.bmp";
+	}
+
+	std::vector<std::string> fileNames = { base_img_path, cmp_img_path };
 
 	// multiple reading of files
 	std::vector<std::shared_ptr<Matrix<Color24b>>> data = extractor.readFiles(fileNames);
@@ -34,7 +54,7 @@ int main()
 
 	am::configuration::ConfigurationReader reader;
 
-	auto conf = reader.getConfigurationFromFile("inputs/configuration.csv");
+	auto conf = reader.getConfigurationFromFile(conf_path);
 	const size_t opt_threads = am::common::getOptimalThreadsCount(conf->ThreadsMultiplier);
 
 	algorithm::ObjectDetector detector = algorithm::ObjectDetector(opt_threads, conf, loggerPtr);
@@ -55,6 +75,6 @@ int main()
 		drawer.drawRectangle(obj.getLeft(), obj.getMinHeight(), obj.getRight(),
 			obj.getMaxHeight());
 	}
-	drawer.save(std::string("compare_result.bmp"));
+	drawer.save(std::string(out_img_path));
 	return 0;
 }
