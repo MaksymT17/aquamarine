a0a9e562b98f8b2460cad6764c0423e0d2ad0134
Maksym Vinnichek <m.vinnichek@gmail.com>
2021-12-11 14:18:03 +0200

    Add gtest linkage for existing tests
    prepare_build.sh - use for reconfiguring UT build.
    make_and_run_tests.sh - build UTs in build folder, use in case of modification existing tests.
    
    All UTs build artifacts will be stored in build/ folder.

--- unit_tests/prepare_build.sh
+++ unit_tests/prepare_build.sh
@@ -1,13 +1,9 @@
-echo "UNIT TESTS SCRIPT:  Removing existing build folder..."
+
+echo "PREPARE UNIT TESTS SCRIPT:  Removing existing build folder..."
 rm -rf build/ &&
 mkdir build &&
 scp -r ../inputs/ build/ &&
-echo "Cloning googletest repo..." &&
-rm -rf googletest &&
-
-if ! git clone https://github.com/google/googletest.git
-then
-  echo "$(tput setaf 1)Cloning of googletest has been failed. The environment is incomplete."
-else
-  echo "$(tput setaf 2)The environment for the unit tests is ready."
-fi
+echo "PREPARE UNIT TESTS SCRIPT: Preparing new build with cmake..." &&
+cd build/ &&
+cmake .. &&
+echo "PREPARE UNIT TEST SRIPT: Cmake generated files for build"
