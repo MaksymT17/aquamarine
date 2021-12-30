8fceb0277debf20495ce35ba45f585107ed62acf
Maksym Vinnichek <m.vinnichek@gmail.com>
2021-12-30 10:57:52 +0200

    Split of Object file to multiple sources
    Split Object to ObjectBase, Object, ObjectRectangle. Curruntly AM uses ObjectRectangle
    for storing objects. In case of AI it may require pixels storage as Object.cpp has, swithcing back is possible.
    
    Cmake: Add compiling of static library for aqumarine. Then this library linking to aquamarine demo.

---
+++ analyze/algorithm/ObjectBase.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <cstdlib>
+
+namespace am
+{
+    namespace analyze
+    {
+        namespace algorithm
+        {
+
+            class ObjectBase
+			{
+			public:
+				explicit ObjectBase(const size_t row, const size_t col) : mPixelsCount(1),
+																		  mLeft(col),
+																		  mMin_height(row),
+																		  mRight(col),
+																		  mMax_height(row) {}
+				bool isMeargableToLeft(ObjectBase &toCompare) const noexcept;
+				bool mergeIfPossibleLeftToMe(ObjectBase &toCompare) noexcept;
+
+				size_t getLeft() const noexcept;
+				size_t getRight() const noexcept;
+				size_t getMinHeight() const noexcept;
+				size_t getMaxHeight() const noexcept;
+				size_t getPixelsCount() const noexcept;
+
+				void printToConsole() const noexcept;
+				void clearPixelsCount() noexcept;
+
+			protected:
+				void mergeToMe(ObjectBase &toCompare) noexcept;
+				size_t mPixelsCount;
+				size_t mLeft;
+				size_t mMin_height;
+				size_t mRight;
+				size_t mMax_height;
+			};
+       
+        }
+    }
+}
