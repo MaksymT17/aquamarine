d75f2e8e54da3de5bb436ab4321723e6b0033259
ben <eagleowlhawk@gmail.com>
2024-04-03 17:52:18 +0300

    AM-65 Fix UT

--- analyze/algorithm/ObjectRectangle.cpp
+++ analyze/algorithm/ObjectRectangle.cpp
@@ -5,6 +5,7 @@ namespace am::analyze::algorithm
     ObjectRectangle::ObjectRectangle(const size_t row, const size_t col)
         : ObjectBase(row, col)
     {
+        addPixel(row,col);
     }
 
     void ObjectRectangle::addPixel(const size_t row, const size_t col)
