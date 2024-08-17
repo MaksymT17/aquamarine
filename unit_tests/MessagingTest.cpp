c6ac8f4abe5713264b43d6590d939a0fd516e601
ben <eagleowlhawk@gmail.com>
2024-08-17 19:39:48 +0300

    Add build of shared_mem lib, add UT

--- unit_tests/MessagingTest.cpp
+++ unit_tests/MessagingTest.cpp
@@ -3,8 +3,8 @@
 #include <thread>
 #include <chrono>
 #include <mutex>
-
-static const std::string shared_mem_name{"/shmsh1"};
+#include "gtest/gtest.h"
+static const std::string shared_mem_name{"/sh_mem_100"};
 
 // Function to be executed in a separate thread
 void backgroundTask()
@@ -15,15 +15,14 @@ void backgroundTask()
 
     int counter = 0;
 
-    while (counter < 200)
+    while (counter < 2)
     {
-        {
-            Message msg{777, MessageType::HANDSHAKE_OK};
-            Message *res = slave->receive();
-            msg.id = res->id;
-            slave->send(&msg);
-            counter++;
-        }
+        Message msg{777, MessageType::HANDSHAKE_OK};
+        std::cout << "Background Slave rec\n";
+        Message *res = slave->receive();
+        msg.id = res->id;
+        slave->send(&msg);
+        counter++;
     }
 
     delete slave;
@@ -38,19 +37,21 @@ void backgroundTaskMasterMaster()
 
     int counter = 0;
 
-    while (counter < 100)
-    {
-
+    while (counter < 1)
+    {std::cout << "Background Master 1send\n";
         master.send(&msg_hand);
+        std::cout << "Background Master rec\n";
         auto msg_resp = master.receive();
-        std::cout << "m 1 =" << msg_resp->id << std::endl;
+        //std::cout << "m 1 =" << msg_resp->id << std::endl;
+        EXPECT_TRUE(msg_resp->id==1);
+        
         counter++;
     }
 
     std::cout << "Background Master task completed.\n";
 }
 
-int main()
+TEST(MessagingTest, TestMultiMasterMode)
 {
     std::cout << "Main thread starts...\n";
     std::vector<int> vec1{1, 2, 3, 4, 5};
@@ -64,19 +65,15 @@ int main()
     // std::this_thread::sleep_for(std::chrono::seconds(5));
     int counter = 0;
 
-    while (counter < 100)
+    while (counter < 1)
     {
         master.send(&msg_hand);
         auto msg_resp = master.receive();
-        // std::cout << "m 2 " << msg_resp->id << std::endl;
+        EXPECT_TRUE(msg_resp->id==2);
         counter++;
     }
-
     if (worker.joinable())
         worker.join();
-
     if (worker_master.joinable())
         worker_master.join();
-
-    return 0;
 }
