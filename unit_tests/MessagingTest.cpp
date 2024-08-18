5fb3b8d83e53916719292bda06c41272c8d8ffab
ben <eagleowlhawk@gmail.com>
2024-08-18 12:56:46 +0300

    Update Messager UT where server starts first

--- unit_tests/MessagingTest.cpp
+++ unit_tests/MessagingTest.cpp
@@ -4,28 +4,25 @@
 #include <chrono>
 #include <mutex>
 #include "gtest/gtest.h"
-static const std::string shared_mem_name{"/sh_mem_100"};
+static const std::string shared_mem_name{"/shmem_test"};
 
 // Function to be executed in a separate thread
 void backgroundTask()
 {
-    std::cout << "Background task started...\n";
-    ProcCommunicator *slave = new ProcCommunicator(false, true, shared_mem_name);
-    Message *res = nullptr;
-
+    std::cout << "Background task starts.\n";
+    Message msg_hand{2, MessageType::HANDSHAKE};
+    ProcCommunicator master(true, true, shared_mem_name);
     int counter = 0;
 
-    while (counter < 2)
+    while (counter < 10)
     {
-        Message msg{777, MessageType::HANDSHAKE_OK};
-        std::cout << "Background Slave rec\n";
-        Message *res = slave->receive();
-        msg.id = res->id;
-        slave->send(&msg);
+        //each client must receive its id
+        master.send(&msg_hand);
+        auto msg_resp = master.receive();
+        EXPECT_EQ(msg_resp->id, 2);
         counter++;
     }
 
-    delete slave;
     std::cout << "Background task completed.\n";
 }
 
@@ -37,43 +34,39 @@ void backgroundTaskMasterMaster()
 
     int counter = 0;
 
-    while (counter < 1)
-    {std::cout << "Background Master 1send\n";
+    while (counter < 10)
+    {
         master.send(&msg_hand);
-        std::cout << "Background Master rec\n";
         auto msg_resp = master.receive();
-        //std::cout << "m 1 =" << msg_resp->id << std::endl;
-        EXPECT_TRUE(msg_resp->id==1);
-        
+        EXPECT_EQ(msg_resp->id, 1);
         counter++;
     }
 
     std::cout << "Background Master task completed.\n";
 }
-
 TEST(MessagingTest, TestMultiMasterMode)
 {
-    std::cout << "Main thread starts...\n";
-    std::vector<int> vec1{1, 2, 3, 4, 5};
+    std::cout << "Server starts...\n";
+    ProcCommunicator *slave = new ProcCommunicator(false, true, shared_mem_name);
+    Message *res = nullptr;
 
-    Message msg_hand{2, MessageType::HANDSHAKE};
-    ProcCommunicator master(true, true, shared_mem_name);
-
+    int counter = 0;
     std::thread worker(backgroundTask);
     std::thread worker_master(backgroundTaskMasterMaster);
-
-    // std::this_thread::sleep_for(std::chrono::seconds(5));
-    int counter = 0;
-
-    while (counter < 1)
+    while (counter < 20)
     {
-        master.send(&msg_hand);
-        auto msg_resp = master.receive();
-        EXPECT_TRUE(msg_resp->id==2);
+        Message msg{777, MessageType::HANDSHAKE_OK};
+        Message *res = slave->receive();
+        msg.id = res->id;
+        slave->send(&msg);
         counter++;
     }
+
+    delete slave;
+
     if (worker.joinable())
         worker.join();
+
     if (worker_master.joinable())
         worker_master.join();
 }
