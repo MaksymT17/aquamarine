22abdc7c940b75f2988e627815aa7e0da7d00971
Max <42316482+MaksymT17@users.noreply.github.com>
2025-09-23 12:23:21 +0300

    AM-81 Build silber as library from sources (#115)
    * AM-80 Fix GHA automatic pipeline builds
    
    * AM-81 Build silber as library from sources
    
    * Update Windows build with cloning silber sources
    
    * Fix pulling silber with 1.2 tag
    
    * Add tag for gtest repo
    
    * Add SB includes in UT
    
    * Update linkage to silber 1.2
    
    * Fix build path
    
    * Update GH runner with silber tag 1.2

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
-echo "removing existing build folder..."
+echo "Removing existing build folder ..."
 rm -rf build/ &&
+echo "Clone silber sources from repo." &&
+rm -rf silber/
+git clone --branch "1.2" --depth 1 https://github.com/MaksymT17/silber &&
 mkdir build &&
 echo "preparing new build with cmake..." &&
 cd build/ &&
 cmake .. &&
-make -j8 &&
+make -j4 &&
 echo "Build complete!"
