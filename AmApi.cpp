d0231097e973114cb474162032926401d0f58915
ben <eagleowlhawk@gmail.com>
2026-06-20 17:20:16 +0300

    Fix: Prevent SQL injection by escaping single quotes

--- AmApi.cpp
+++ AmApi.cpp
@@ -40,8 +40,20 @@ namespace am
 #ifndef WIN32
         if (dbcPtr)
         {
+            std::string safe_base = base_img;
+            std::string safe_cmp = cmp_img;
+            auto sanitize = [](std::string& str) {
+                size_t pos = 0;
+                while ((pos = str.find("'", pos)) != std::string::npos) {
+                    str.replace(pos, 1, "''");
+                    pos += 2;
+                }
+            };
+            sanitize(safe_base);
+            sanitize(safe_cmp);
+
             std::stringstream ss;
-            ss << "'" << am::common::get_datetime() << "', '" << base_img << "', '" << cmp_img << "', " << objects.size();
+            ss << "'" << am::common::get_datetime() << "', '" << safe_base << "', '" << safe_cmp << "', " << objects.size();
             dbcPtr->add_results_table(); // if already exists - it produces a warning
             dbcPtr->insert_records2results({ss.str()});
         }
